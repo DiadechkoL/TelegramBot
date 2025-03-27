bbd25fc434bed1f8ef539941a65ec909d44013e5
DiadechkoL <liza.dyadechko@gmail.com>
2025-03-27 13:02:22 +0100

    url

--- src/modules/user/user.model.js
+++
@@ -1,7 +0,0 @@
-// // src/models/user.model.ts
-// export interface UserModel {
-//     id: string; // Уникальный идентификатор пользователя
-//     phone_id: string; // Идентификатор телефона
-//     balance: number;
-// }
-//# sourceMappingURL=user.model.js.map
